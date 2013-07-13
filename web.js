691c02e91452e7d88a400a4df9994dc36e8e9943
Ubuntu <ubuntu@ip-172-31-42-161.us-west-2.compute.internal>
2013-07-13 06:12:44 +0000

    should be it

--- web.js
+++ web.js
@@ -1,12 +1,33 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+
+
+//var theText="Hi!";
+var infil ='index.html';
+var theText = fs.readFileSync(infil, 'utf8');
+//fs.readFileSync('index.html');
+//theText=fs.readFileSync('index.html','utf8')
+//, function (err, data) {
+ //   if (err) throw err;
+ //   theText= data;
+//})
+//; 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(theText);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+//fs.writeFileSync(outfile, fmt(firstnprimes(n)));
+
+
